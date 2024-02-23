90652bedd39d007f2f390bf05d1e82c5c7337426
Abinash <abinashbehera9898@gmail.com>
2024-02-23 09:05:35 +0530

    adeed Test.java to DEMO/JAVA

---
+++ DEMO/src/JAVA/Test.java
@@ -0,0 +1,16 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter your number");
+      int num =  sc.nextInt();
+      System.out.print(num);
+     
+	}
+
+}
