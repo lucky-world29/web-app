60c244fa94f40a896d34222ad591ad1af565cb6e
Abinash <abinashbehera9898@gmail.com>
2024-08-01 00:23:54 +0530

    obj.js in JS added

---
+++ JavaScript/console_practice/obj.js
@@ -0,0 +1,14 @@
+const fruits = [
+    ["apples",300],
+    ["oranges",200],
+    ["bananas",100],
+]
+
+//console.log(Object.fromEntries(fruits));
+
+const fruitCounts = fruits.reduce((acc, [fruit, count]) => {
+    acc[fruit] = (acc[fruit] || 0) + count;
+    return acc;
+}, {});
+
+console.log(fruitCounts)
