01cfb50ece902c11dedb84d94fcb18df731544de
Abinash <abinashbehera9898@gmail.com>
2024-08-15 16:39:25 +0530

    05 string.js is complete

--- JavaScript/Chai-aur-Code/05-string.js
+++ JavaScript/Chai-aur-Code/05-string.js
@@ -8,6 +8,29 @@ const repoCount = 50;
 const my_name = new String('Bob');
 //console.log(my_name);
 
-console.log(my_name[0]);
-console.log(my_name.__proto__);
+// console.log(my_name[0]);
+// console.log(my_name.__proto__);
 
+// const gameName = "Hitesh";
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-1,7);
+// console.log(anotherString);
+
+// //trim  and replace 
+
+// const newStringOne = "    lucky        .";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "www.linkedin.com/in/abinash-behera-200145255%29 jb";
+console.log(url);
+
+const name2 = "The quick brown fox jumps over the lazy dog.  ";
+const name3 = console.log(name2.split(''));
+console.log(name3[3]);
