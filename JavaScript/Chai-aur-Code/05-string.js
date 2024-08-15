b17086ba642afd0d87fc5baea3e112320bbb0fa7
Abinash <abinashbehera9898@gmail.com>
2024-08-15 11:49:42 +0530

    .

--- JavaScript/Chai-aur-Code/05-string.js
+++ JavaScript/Chai-aur-Code/05-string.js
@@ -3,7 +3,11 @@ const repoCount = 50;
 
 //console.log(name + " "+ repoCount+" "+"values");
 
-console.log(`my name is ${name} and my repo count is ${repoCount}`);
+//console.log(`my name is ${name} and my repo count is ${repoCount}`);
 
 const my_name = new String('Bob');
-console.log(my_name);
+//console.log(my_name);
+
+console.log(my_name[0]);
+console.log(my_name.__proto__);
+
