2197d3e6eeaf7407c615deaa49cfae3680190884
Abinash <abinashbehera9898@gmail.com>
2024-08-17 23:55:55 +0530

    some modification and date is done

---
+++ JavaScript/Chai-aur-Code/01-Basic/07-dates.js
@@ -0,0 +1,17 @@
+// Dates 
+/*
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+*/
+// let myCreatedDate = new Date(2023,0,23);
+// let myCreatedDate = new Date(2023,0,23,10,10,10); // to print all of this we need to use toLocaleString() .
+// console.log(myCreatedDate.toLocaleString());
+
+let myCreateDate1 = new Date("29-11-2001")
+console.log(myCreateDate1.toDateString());
+
+
+let 
