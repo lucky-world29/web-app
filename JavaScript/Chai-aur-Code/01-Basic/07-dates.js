8ce96e6c5fe63df94411f5d81141ccb5f97cae61
Abinash <abinashbehera9898@gmail.com>
2024-09-04 20:48:20 +0530

    07-datse.js completed

--- JavaScript/Chai-aur-Code/01-Basic/07-dates.js
+++ JavaScript/Chai-aur-Code/01-Basic/07-dates.js
@@ -8,30 +8,31 @@ console.log(myDate.toLocaleString());
 */
 // let myCreatedDate = new Date(2023,0,23);
 // let myCreatedDate = new Date(2023,0,23,10,10,10); // to print all of this we need to use toLocaleString() .
-// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toDateString()+"  h");
 
-let myCreateDate1 = new Date("03-11-2001"); // MM-DD-YYYY
-console.log(myCreateDate1.toLocaleString());
+// let myCreateDate1 = new Date("03-11-2001"); // MM-DD-YYYY
+// console.log(myCreateDate1.toLocaleString());
 
-let myCreateDate2 = new Date("2023-02-23"); // YYYY-MM-DD
-console.log(myCreateDate2.toDateString());
-
-
-let myTimeStamp = Date.now();
-console.log(myTimeStamp);
-// console.log(myCreateDate1.getTme()); .. // wrong or what 
-console.log(Math.floor(Date.now()/1000));
+// let myCreateDate2 = new Date("2023-02-23"); // YYYY-MM-DD
+// console.log(myCreateDate2.toDateString());
 
 
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// // console.log(myCreateDate1.getTme()); .. // wrong or what 
+// console.log(Math.floor(Date.now()/1000));
 
 
-let date = new Date();
-console.log(date.toJSON());
-console.log(date.toLocaleDateString());
+
+
+// let date = new Date();
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
 
 
 
 let newDate = new Date();
 console.log(newDate);
 
-
+let x = `Today's date is ${newDate.getDay()+1} and time is ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()} `
+console.log(x);
