678c7fb6f13b599dddb3154517fa0ff5a387a19e
Abinash <abinashbehera9898@gmail.com>
2024-09-06 19:59:09 +0530

    01-array.js completed

---
+++ JavaScript/Chai-aur-Code/02-Basic/01-arrays.js
@@ -0,0 +1,36 @@
+
+const myArr = [1, 2, 3, 4, 5];
+
+// console.log(myArr[0]);
+
+
+// Array methods
+
+// myArr.push(39);
+// console.log(myArr);
+
+// myArr.pop();
+
+// myArr.unshift(8);
+// myArr.shift();
+
+// console.log(myArr.includes('a'));
+// console.log(myArr.indexOf(90));
+
+// const newArr = myArr.join();
+// console.log(newArr);
+// console.log(typeof newArr);
+
+console.log("A " + myArr);
+
+//SLICE
+const myn1 = myArr.slice(0,2);
+console.log(myn1);
+console.log("B "+myArr);
+
+
+//SPLICE
+const myn2 = myArr.splice(0,2);
+console.log(myn2);
+console.log("C "+myArr);
+
