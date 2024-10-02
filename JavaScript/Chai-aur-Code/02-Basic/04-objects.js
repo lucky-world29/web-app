40c8ab76794cbcb6dae7d2766864f23aa79f8059
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-10-03 00:18:43 +0530

    Object in JS Completed

--- JavaScript/Chai-aur-Code/02-Basic/04-objects.js
+++ JavaScript/Chai-aur-Code/02-Basic/04-objects.js
@@ -22,8 +22,50 @@ const regularUser = {
 const obj1 = {1: "a", 2: "b", 3: "c"}
 const obj2 = {4: "d", 5: "e", 6: "f"}
 
-const obj3 = {...obj1, ...obj2}
-console.log(obj3)
+const obj3 = {...obj1, ...obj2}// spread operator
+// console.log(obj3)
 const obj4 = {obj1, obj2}
-console.log(obj4)
+// console.log(obj4)
+
+const obj5 = Object.assign({},obj1, obj2)
+// console.log(obj5)
+
+const users =[
+    {
+        id : 1,
+        email : "lucky@google.com",
+    },
+    {
+        id : 2,
+        email : "abinash@google.com",
+    }
+]
+
+// console.log(users.push(
+//     {
+//         id : 3, 
+//         email : "hitesh@google.com"
+//     })
+// );
+
+users.push({
+    id :11,
+    name : "Soul"
+});
+
+// console.log(users)
+
+users[1].email
+/* */
+/*
+console.log(tinderUser)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+*/
+/* */
+// console.log(Object.keys(tinderUser));
+
+// console.log(typeof(tinderUser))
+
 
